ba48be05046d64a3e3c97dd18684fd61c6661cff
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2023-09-03 17:19:28 -0300

    feat(utils): add text formatting options

--- src/heimdallr/utils/formatting.py
+++ src/heimdallr/utils/formatting.py
@@ -1,6 +1,7 @@
 """
 Formatting utilities.
 """
+import re
 from re import sub
 
 
@@ -13,3 +14,47 @@ def to_camel(s: str) -> str:
     """
     s = sub(r"(_|-)+", " ", s).title().replace(" ", "")
     return "".join([s[0].lower(), s[1:]])
+
+
+def contains_letters_or_numbers(text: str):
+    """
+    Checks if a paragraph contains letters or numbers.
+
+    Args:
+        text (str): A paragraph of text.
+
+    Returns:
+        bool: True if the paragraph contains letters or numbers, False otherwise.
+    """
+    pattern = re.compile(r"[a-zA-Z0-9]")
+    return bool(pattern.search(text))
+
+
+def normalize_sentence(text: str) -> str:
+    """
+    Normalizes a sentence that has been split into multiple lines, matching the following criteria:
+
+    * If the first character of a line is lowercase, it is part of the previous sentence.
+    * Else the line is part of the next sentence.
+
+
+    Args:
+        text (str): A text to be normalized.
+
+    Returns:
+        str: The formatted text.
+    """
+    splits = text.split("\n")
+
+    normalized_sentence = ""
+
+    for split in splits:
+        line = split.strip()
+
+        if line:
+            if line[0].islower():
+                normalized_sentence += line + " "
+            else:
+                normalized_sentence = normalized_sentence.strip() + "\n" + line + " "
+
+    return normalized_sentence.strip()
--- tests/unit/utils/test_formatting.py
+++ tests/unit/utils/test_formatting.py
@@ -1,10 +1,14 @@
 """
 Formatting utilities tests.
 """
-from heimdallr.utils.formatting import to_camel
+from heimdallr.utils.formatting import (
+    contains_letters_or_numbers,
+    normalize_sentence,
+    to_camel,
+)
 
 
-class TestFormatting:
+class TestToCamel:
     """
     Formatting utilities tests cases.
     """
@@ -58,3 +62,71 @@ class TestFormatting:
         foo = "foo_bar-baz"
 
         assert to_camel(foo) == "fooBarBaz"
+
+
+class TestContainsAlphanumeric:
+    def test_contains_alphanumeric(self):
+        """
+        GIVEN a string with alphanumeric characters
+        WHEN the string is checked for alphanumeric characters
+        THEN assert the string contains alphanumeric characters
+        """
+        foo = "foo123"
+
+        assert contains_letters_or_numbers(foo)
+
+    def test_contains_non_alphanumeric(self):
+        """
+        GIVEN a string with non-alphanumeric characters
+        WHEN the string is checked for alphanumeric characters
+        THEN assert the string does not contain alphanumeric characters
+        """
+        foo = "!@#%"
+
+        assert not contains_letters_or_numbers(foo)
+
+    def test_contains_alphanumeric_and_non_alphanumeric(self):
+        """
+        GIVEN a string with alphanumeric and non-alphanumeric characters
+        WHEN the string is checked for alphanumeric characters
+        THEN assert the string contains alphanumeric characters
+        """
+        foo = "foo123!@#%"
+
+        assert contains_letters_or_numbers(foo)
+
+
+class TestTextNormalizing:
+    def test_normalize_sentence(self):
+        """
+        GIVEN a sentence that has been split into multiple lines
+        WHEN the sentence is normalized
+        THEN assert the sentence is normalized correctly
+        """
+        sentence = "This is a sentence.\nThis is another sentence."
+
+        assert normalize_sentence(sentence) == "This is a sentence.\nThis is another sentence."
+
+    def test_normalize_sentence_with_lowercase(self):
+        """
+        GIVEN a sentence that has been split into multiple lines
+        WHEN the sentence is normalized
+        THEN assert the sentence is normalized correctly
+        """
+        sentence = "This is a sentence\nthat is continued into another line."
+
+        assert normalize_sentence(sentence) == "This is a sentence that is continued into another line."
+
+    def test_normalize_sentence_with_lowercase_and_uppercase(self):
+        """
+        GIVEN a sentence that has been split into multiple lines
+        WHEN the sentence is normalized
+        THEN assert the sentence is normalized correctly
+        """
+        line_0 = "This is a sentence\nthat is continued into another line."
+        line_1 = " THIS SENTENCE IS CAPITALIZED IN THE SAME LINE."
+        line_2 = "\nHowever, this uses another line."
+
+        sentence = line_0 + line_1 + line_2
+
+        assert normalize_sentence(sentence) != sentence
