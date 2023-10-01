15ff905ffe26a7e803ad02f93c772c6a2c7dd1e0
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2023-10-01 17:26:29 -0300

    feat(adapters): add assignment reader

---
+++ src/heimdallr/adapters/assignment_reader.py
@@ -0,0 +1,66 @@
+"""
+Adapter for reading assignments from a file.
+"""
+import abc
+import datetime
+from typing import BinaryIO
+
+import fitz
+from spacy import Language
+
+from heimdallr.domain.models.assignment import Assignment, Page
+from heimdallr.utils.formatting import contains_letters_or_numbers, normalize_sentence
+
+
+class AssignmentReader(abc.ABC):
+    @abc.abstractmethod
+    def read(self, file_ref: str | BinaryIO, file_type: str) -> Assignment:
+        """
+        Parses a file into a domain model.
+
+        Args:
+            file_ref (str | BinaryIO): Either a file path or a file stream.
+            file_type (str): The file type.
+
+        Returns:
+            Assignment: A mapped document.
+        """
+        raise NotImplementedError
+
+
+class SpacyAssignmentReader(AssignmentReader):
+    def __init__(self, nlp: Language):
+        """
+        A PDF Reader that maps the document into a domain model.
+
+        Args:
+            nlp (Language): The Natural Language Processor.
+        """
+        self.nlp = nlp
+
+    def read(self, file_ref: str | BinaryIO, file_type: str = "pdf") -> Assignment:
+        args: dict[str, str | bytes] = {"filetype": file_type}
+
+        if isinstance(file_ref, str):
+            args["filename"] = file_ref
+        else:
+            args["stream"] = file_ref.read()
+
+        file_document = fitz.Document(**args)
+
+        pages: list[Page] = [self._parse_page(page) for page in file_document]
+
+        file_document.close()
+
+        return Assignment(content=pages, date=datetime.date.today())
+
+    def _parse_page(self, page: fitz.Page) -> list[str]:
+        text: str = page.get_textpage().extractText()
+
+        # Parse the page text into a spacy document
+        doc = self.nlp(text)
+
+        # Split the page text into sentences
+        page_sentences = [normalize_sentence(str(s)) for s in doc.sents if contains_letters_or_numbers(str(s))]
+
+        return page_sentences
