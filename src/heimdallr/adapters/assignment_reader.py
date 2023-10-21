2f7302c413aeeaf777c7a9f54f6200878016de5b
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2023-10-21 11:02:41 -0300

    refactor(adapters): different methods for file types

--- src/heimdallr/adapters/assignment_reader.py
+++ src/heimdallr/adapters/assignment_reader.py
@@ -9,6 +9,7 @@ import fitz
 from spacy import Language
 
 from heimdallr.domain.models.assignment import Assignment, Page
+from heimdallr.utils.content_type import APPLICATION_PDF
 from heimdallr.utils.formatting import contains_letters_or_numbers, normalize_sentence
 
 
@@ -61,7 +62,22 @@ class SpacyAssignmentReader(AssignmentReader):
         self.nlp = nlp
 
     def read(self, file_ref: str | BinaryIO, file_type: str = "pdf") -> Assignment:
-        args: dict[str, str | bytes] = {"filetype": file_type}
+        if file_type == APPLICATION_PDF:
+            return self.read_pdf(file_ref)
+
+        return Assignment(content=[], date=datetime.date.today(), author="Unknown")
+
+    def read_pdf(self, file_ref: str | BinaryIO) -> Assignment:
+        """
+        Parses a PDF file into a domain model.
+
+        Args:
+            file_ref (str | BinaryIO): Either a file path or a file stream.
+
+        Returns:
+            Assignment: A mapped document.
+        """
+        args: dict[str, str | bytes] = {"filetype": "pdf"}
 
         if isinstance(file_ref, str):
             args["filename"] = file_ref
@@ -70,37 +86,44 @@ class SpacyAssignmentReader(AssignmentReader):
 
         file_document = fitz.Document(**args)
 
-        pages: list[Page] = [self._parse_page(page) for page in file_document]
+        pages: list[Page] = [self._parse_page(page.get_textpage().extractText()) for page in file_document]
 
-        author = self._find_out_author(file_document[0])
+        author = self._find_out_author(file_document[0].get_textpage().extractText())
 
         file_document.close()
 
         return Assignment(content=pages, date=datetime.date.today(), author=author)
 
-    def _parse_page(self, page: fitz.Page) -> list[str]:
-        text: str = page.get_textpage().extractText()
+    def _parse_page(self, page_text: str) -> list[str]:
+        """
+        Parses string from a page into a list of sentences.
+
+        Args:
+            page_text: the string to be parsed.
+
+        Returns:
+            list[str]: A list of sentences.
+        """
 
         # Parse the page text into a spacy document
-        doc = self.nlp(text)
+        doc = self.nlp(page_text)
 
         # Split the page text into sentences
         page_sentences = [normalize_sentence(str(s)) for s in doc.sents if contains_letters_or_numbers(str(s))]
 
         return page_sentences
 
-    def _find_out_author(self, page: fitz.Page) -> str:
+    def _find_out_author(self, page_text: str) -> str:
         """
         Finds out the author of the assignment.
         It supposes that the document contains a page with the author's name on it.
 
         Args:
-            page: The first page of the assignment.
+            page_text: The first page of the assignment.
 
         Returns:
             str: The author's name.
         """
-        page_text = page.get_textpage().extractText()
 
         doc = self.nlp(page_text)
 
