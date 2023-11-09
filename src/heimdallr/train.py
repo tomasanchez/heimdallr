850a56fc257ea798d6ef3eb3c3eafbce63bce6fd
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2023-11-09 00:34:21 -0300

    build(model): model training

---
+++ src/heimdallr/train.py
@@ -0,0 +1,44 @@
+"""Train
+Script to train the model.
+"""
+import os
+
+import joblib
+import pandas as pd
+import spacy
+from sklearn.model_selection import train_test_split
+
+from heimdallr.adapters.assignment_reader import SklearnTopicPredictor
+from heimdallr.dependencies import NLP_SPANISH
+
+# convert to pandas dataframe
+
+
+if __name__ == "__main__":
+    print("Training the model...")
+    # train the model
+    upper_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+    path = os.path.join(upper_dir, "db", "training.csv")
+    df = pd.read_csv(path)
+
+    # create train and test set
+    train, test = train_test_split(df, test_size=0.33, random_state=42)
+    print("Training Data Shape:", train.shape)
+    print("Testing Data Shape:", test.shape)
+
+    # convert to list
+    content_list = train["content"].tolist()
+    train1 = []
+    for sentences in content_list:
+        train1.append("".join(sentences))
+    labelsTrain1 = train["topic"].tolist()
+
+    # load spaCy
+    nlp = spacy.load(NLP_SPANISH)
+    topic_predictor = SklearnTopicPredictor(nlp=nlp, download=True)
+    topic_predictor.pipeline.fit(train1, labelsTrain1)
+
+    # save the model
+    model_path = os.path.join(upper_dir, "models", "topic_predictor.joblib")
+    joblib.dump(topic_predictor.pipeline, model_path)
+    print("Model trained and saved!")
